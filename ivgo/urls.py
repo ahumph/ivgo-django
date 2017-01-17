c3f2b011a4072abf3acdebe4d35dd34484366ee9
Adam Humphreys <a.humph@hotmail.com>
2017-01-17 17:24:27 +0000

    Added root url.

--- ivgo/urls.py
+++ ivgo/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from ivgo import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
 ]
